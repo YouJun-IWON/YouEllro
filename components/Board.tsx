e04aa829aea6c6e2ce86a8bfd1d2a51a2c510cd1
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-09-13 09:34:47 +0900

    set header and appwrite and background

---
+++ components/Board.tsx
@@ -0,0 +1,16 @@
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
+
+const Board = () => {
+  return (
+    <DragDropContext>
+
+      <Droppable droppableId='board' direction='horizontal' type='column'>
+        {(provided) => <div></div>}
+      </Droppable >
+
+    </DragDropContext>
+  )
+}
+
+export default Board
