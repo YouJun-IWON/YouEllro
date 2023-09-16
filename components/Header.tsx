b39a2073008440729c90e7b236b7c3994a7223a3
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-09-16 14:41:17 +0900

    logo image

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,8 @@ import { useBoardStore } from '@/store/BoardStore';
 import { useEffect, useState } from 'react';
 import fetchSuggestion from '@/lib/fetchSuggestion';
 
+import youRello from '../image/YouRello-logos_black.png';
+
 const Header = () => {
   const [board, searchString, setSearchString] = useBoardStore((state) => [
     state.board,
@@ -15,7 +17,7 @@ const Header = () => {
   ]);
 
   const [loading, setLoading] = useState<boolean>(false);
-  const [suggestion, setSuggestion] = useState<string>("")
+  const [suggestion, setSuggestion] = useState<string>('');
 
   useEffect(() => {
     if (board.columns.size === 0) return;
@@ -25,23 +27,21 @@ const Header = () => {
       const suggestion = await fetchSuggestion(board);
       setSuggestion(suggestion);
       setLoading(false);
-    }
+    };
 
     fetchSuggestionFunc();
-  }, [board])
-
-
+  }, [board]);
 
   return (
     <header>
       <div className='flex flex-col md:flex-row items-center p-5  bg-gray-500/10 rounded-b-2xl'>
         <div className='absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50'></div>
         <Image
-          src='https://links.papareact.com/c2cdd5'
+          src={youRello}
           alt='Trello Logo'
-          width={150}
-          height={100}
-          className='w-44 md:w-56 pb-10 md:pb-0 object-contain '
+          width={200}
+          height={150}
+          className='w-44 h-20 md:w-56 pb-10 md:pb-0 object-cover scale-150'
         ></Image>
 
         <div className='flex items-center space-x-5 flex-1 justify-end w-full'>
@@ -68,10 +68,14 @@ const Header = () => {
 
       <div className='flex items-center justify-center px-5 py-2 md:py-5'>
         <p className='flex items-center text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1] p-5'>
-          <UserCircleIcon className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${loading && "animate-spin"}`} />
-          {
-            suggestion && !loading ? suggestion : "GPT is summarizing your tasks for the day..."
-          }
+          <UserCircleIcon
+            className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${
+              loading && 'animate-spin'
+            }`}
+          />
+          {suggestion && !loading
+            ? suggestion
+            : 'GPT is summarizing your tasks for the day...'}
         </p>
       </div>
     </header>
